61d90571d09e9ef64ce0c1dd7a484f291f70f35f
Raveen Pramuditha <raveenpspathirana99@gmail.com>
2024-09-16 23:03:56 +0530

    sec

--- floodfill.cpp
+++ floodfill.cpp
@@ -14,6 +14,20 @@ struct floodfill
     bool westwall;
 };
 
+// make array 16x16
+floodfill maze[16][16];
+
+// make a queue
+queue<pair<int, int>> q;
+
+// make a function to check if the cell is valid
+bool isvalid(int x, int y){
+    if(x>=0 && x<16 && y>=0 && y<16){
+        return true;
+    }
+    return false;
+}
+
 int main(){
     
     return 0;
